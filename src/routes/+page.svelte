ffc3f77bc86d1dfd9a7d1c227bbd0b9db8729ad9
Zelun Zhang <zelunzhangofficial@gmail.com>
2024-11-20 21:22:19 +1030

    Hero.svelte: Add Header tag in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-  import Header from "../components/Header.svelte";
+  import Hero from "../components/Hero.svelte";
 </script>
 
 <main class="flex flex-col">
-  <Header />
+  <Hero />
 </main>
