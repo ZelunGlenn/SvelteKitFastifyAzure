8186af2cf42bfb320388a5822e925f4cd72e4ca6
Zelun Zhang <zelunzhangofficial@gmail.com>
2024-11-20 23:05:35 +1030

    Header: logo, nav, burger (mobile)

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,27 @@
 <script>
-  import SectionWrapper from "./SectionWrapper.svelte";
 </script>
 
+<header class="flex flex-col relative z-20">
+  <div
+    class="max-w-[1400px] mx-auto w-full flex items-center
+  justify-between p-4 py-6"
+  >
+    <a href="/">
+      <h1 class="font-semibold">Zelun 
+        <span class="text-indigo-400">Zhang</span>  
+      </h1>
+    </a>
+    <button class="md:hidden grid place-items-center">
+      <i class="fa-solid fa-bars"></i>
+    </button>
+    <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+      <a href="#product" class="duration-200 hover:text-indigo-500 cursor-pointer">Product</a>
+      <a href="#reviews" class="duration-200 hover:text-indigo-500 cursor-pointer">Reviews</a>
+      <a href="#faqs" class="duration-200 hover:text-indigo-500 cursor-pointer">FAQ</a>
+      <button class="specialBtn">
+        <p>Start free today</p>
+      </button>
 
-<SectionWrapper>
-  
-</SectionWrapper>
+    </nav>
+  </div>
+</header>
