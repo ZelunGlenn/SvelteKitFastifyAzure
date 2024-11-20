461c1599844835b28015f57060298b3923b7018d
Zelun Zhang <zelunzhangofficial@gmail.com>
2024-11-20 23:24:22 +1030

    Update hero.svelte: Basic wording

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -6,4 +6,19 @@
 
 <SectionWrapper>
   <Header />
+  <div class="flex flex-col gap-10 flex-1 items-center justify-center pd-10 md:pd-14">
+    <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold">
+      <span class="text-indigo-400">Gym</span> 
+      Training <span class="text-indigo-600 line-through">Is Hard</span><br>
+      Just Got <span class="text-indigo-400">Easier</span>
+    </h2>
+    <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] mx-auto w-full">
+      Less thinking & more doing. We're the trainer in your pocket
+      full of <span class="italic">
+        personalized workouts, excercise and explanations, analytics
+      </span>
+      and much much more.
+    </p>
+
+  </div>
 </SectionWrapper>
